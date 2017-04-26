e73df4d0f9ba79179717307377f0c363e210ceae
Ray Miller <ray@1729.org.uk>
2017-04-26 20:13:04 +0100

    Checkpoint.

--- set1.go
+++ set1.go
@@ -1,183 +1,332 @@
 package set1
 
 import (
-    "bufio"
-    "encoding/base64"
-    "encoding/hex"
-    "log"
-    "os"
-    "unicode"
+	"bufio"
+	"encoding/base64"
+	"encoding/hex"
+	"io"
+	"log"
+	//"math"
+	"os"
+	"sort"
+	"unicode"
+
+	"github.com/montanaflynn/stats"
 )
 
 func hex_bytes(s string) []byte {
-    bytes, err := hex.DecodeString(s)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return bytes
+	bytes, err := hex.DecodeString(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bytes
 }
 
 func hex_to_base64(s string) string {
-    return base64.StdEncoding.EncodeToString(hex_bytes(s))
+	return base64.StdEncoding.EncodeToString(hex_bytes(s))
 }
 
 func fixed_xor(s, k string) string {
-    s_bytes := hex_bytes(s)
-    k_bytes := hex_bytes(k)
-    n := len(s_bytes)
-    if len(k_bytes) != n {
-        log.Fatalf("Inputs to fixed_xor must be the same length")
-    }
-    res := make([]byte, n)
-    for i := 0; i < n; i++ {
-        res[i] = s_bytes[i]^k_bytes[i]
-    }
-    return hex.EncodeToString(res)
+	s_bytes := hex_bytes(s)
+	k_bytes := hex_bytes(k)
+	n := len(s_bytes)
+	if len(k_bytes) != n {
+		log.Fatalf("Inputs to fixed_xor must be the same length")
+	}
+	res := make([]byte, n)
+	for i := 0; i < n; i++ {
+		res[i] = s_bytes[i] ^ k_bytes[i]
+	}
+	return hex.EncodeToString(res)
 }
 
-func single_byte_xor(b byte, s string) string {
-    s_bytes := hex_bytes(s)
-    n := len(s_bytes)
-    res := make([]byte, n)
-    for i := 0; i < n; i++ {
-        res[i] = s_bytes[i]^b
-    }
-    return string(res)
+func single_byte_xor(b byte, s_bytes []byte) []byte {
+	n := len(s_bytes)
+	res := make([]byte, n)
+	for i := 0; i < n; i++ {
+		res[i] = s_bytes[i] ^ b
+	}
+	return res
 }
 
 /* Letter frequencies from
 
- https://en.wikipedia.org/wiki/Letter_frequency
+https://en.wikipedia.org/wiki/Letter_frequency
 
- I took a guess at 13% for space as wp just says "In English, the
- space is slightly more frequent than the top letter (e)..."
+I took a guess at 13% for space as wp just says "In English, the
+space is slightly more frequent than the top letter (e)..."
 
- It might be useful to consider n-grams, e.g. http://norvig.com/mayzner.html
- or tokenize the string and score against a dictionary.
+It might be useful to consider n-grams, e.g. http://norvig.com/mayzner.html
+or tokenize the string and score against a dictionary.
 */
 
 var EnglishLetterFrequency = map[rune]float64{
-    'a': 8.167,
-    'b': 1.492,
-    'c': 2.782,
-    'd': 4.253,
-    'e': 12.702,
-    'f': 2.228,
-    'g': 2.015,
-    'h': 6.094,
-    'i': 6.966,
-    'j': 0.153,
-    'k': 0.772,
-    'l': 4.205,
-    'm': 2.406,
-    'n': 6.749,
-    'o': 7.507,
-    'p': 1.929,
-    'q': 0.095,
-    'r': 5.987,
-    's': 6.327,
-    't': 9.056,
-    'u': 2.758,
-    'v': 0.978,
-    'w': 2.360,
-    'x': 0.150,
-    'y': 1.974,
-    'z': 0.074,
-    ' ': 13.0,
+	'a': 8.167,
+	'b': 1.492,
+	'c': 2.782,
+	'd': 4.253,
+	'e': 12.702,
+	'f': 2.228,
+	'g': 2.015,
+	'h': 6.094,
+	'i': 6.966,
+	'j': 0.153,
+	'k': 0.772,
+	'l': 4.205,
+	'm': 2.406,
+	'n': 6.749,
+	'o': 7.507,
+	'p': 1.929,
+	'q': 0.095,
+	'r': 5.987,
+	's': 6.327,
+	't': 9.056,
+	'u': 2.758,
+	'v': 0.978,
+	'w': 2.360,
+	'x': 0.150,
+	'y': 1.974,
+	'z': 0.074,
+	' ': 13.0,
 }
 
 func score(s string) float64 {
-    frequencies := make(map[rune]int)
-    n := 0
-    for _, c := range s {
-        n++
-        frequencies[unicode.ToLower(c)]++
-    }
-    distance := 0.0
-    // Score English letter characters (and space)
-    for c, ef := range(EnglishLetterFrequency) {
-        f := float64(frequencies[c])*100.0/float64(n)
-        d := ef - f
-        distance += d*d
-    }
-    // Penalize non-ascii characters that appear in `s`
-    // for c, f := range(frequencies) {
-    //     if _, ok := EnglishLetterFrequency[c]; !ok {
-    //         distance += 100.0*float64(f)
-    //     }
-    // }
-    return 1.0/(1.0+distance)
+	frequencies := make(map[rune]int)
+	n := 0
+	for _, c := range s {
+		n++
+		frequencies[unicode.ToLower(c)]++
+	}
+	distance := 0.0
+	// Score English letter characters (and space)
+	for c, ef := range EnglishLetterFrequency {
+		f := float64(frequencies[c]) * 100.0 / float64(n)
+		d := ef - f
+		distance += d * d
+	}
+	// Penalize non-ascii characters that appear in `s`
+	// for c, f := range(frequencies) {
+	//     if _, ok := EnglishLetterFrequency[c]; !ok {
+	//         distance += 100.0*float64(f)
+	//     }
+	// }
+	return 1.0 / (1.0 + distance)
 }
 
-func solve_single_byte_xor(s string) (string, float64) {
-    best := ""
-    best_score := 0.0
-    for b := byte(0); b < 255; b++ {
-        t := single_byte_xor(b, s)
-        s := score(t)
-        if s > best_score {
-            best = t
-            best_score = s
-        }
-    }
-    return best, best_score
+func letter_freq_correlation(s string) float64 {
+	frequencies := make(map[rune]float64)
+	n := 0.0
+	for _, c := range s {
+		n++
+		lc := unicode.ToLower(c)
+		frequencies[lc]++
+	}
+	var xs, ys stats.Float64Data
+	for char, freq := range EnglishLetterFrequency {
+		xs = append(xs, freq)
+		ys = append(ys, frequencies[char]/n)
+	}
+	for char, count := range frequencies {
+		if _, ok := EnglishLetterFrequency[char]; ok {
+			continue
+		}
+		xs = append(xs, 0.0)
+		ys = append(ys, count/n)
+	}
+	correlation, err := xs.Correlation(ys)
+	if err != nil {
+		log.Fatalf("Error computing correlation: %v", err)
+	}
+	return correlation
+}
+
+func solve_single_byte_xor(s []byte) ([]byte, byte, float64) {
+	var best []byte
+	var key byte
+	best_score := 0.0
+	for b := byte(0); b < 255; b++ {
+		t := single_byte_xor(b, s)
+		s := letter_freq_correlation(string(t))
+		if s > best_score {
+			best = t
+			key = b
+			best_score = s
+		}
+	}
+	return best, key, best_score
 }
 
 func detect_single_character_xor(filename string) string {
-    f, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
-    scanner := bufio.NewScanner(f)
-    best := ""
-    best_score := 0.0
-    for scanner.Scan() {
-        this, this_score := solve_single_byte_xor(scanner.Text())
-        if this_score > best_score {
-            best = this
-            best_score = this_score
-        }
-    }
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-    return best
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var best []byte
+	best_score := 0.0
+	for scanner.Scan() {
+		cyphertext, _ := hex.DecodeString(scanner.Text())
+		this, _, this_score := solve_single_byte_xor(cyphertext)
+		if this_score > best_score {
+			best = this
+			best_score = this_score
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return string(best)
 }
 
-func repeating_key_xor(s, k string) string {
-    s_bytes := []byte(s)
-    k_bytes := []byte(k)
-    s_len := len(s_bytes)
-    k_len := len(k_bytes)
-    res := make([]byte, s_len)
-    for i := 0; i < s_len; i++ {
-        res[i] = s_bytes[i]^k_bytes[i % k_len]
-    }
-    return hex.EncodeToString(res)
+func repeating_key_xor(s_bytes, k_bytes []byte) []byte {
+	s_len := len(s_bytes)
+	k_len := len(k_bytes)
+	res := make([]byte, s_len)
+	for i := 0; i < s_len; i++ {
+		res[i] = s_bytes[i] ^ k_bytes[i%k_len]
+	}
+	return res
 }
-
 
 // From "The Go Programming Language", p45
 var pc [256]byte
 
 func init() {
-    for i := range pc {
-        pc[i] = pc[i/2] + byte(i&1)
-    }
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
 }
 
-func hamming_distance(s, t string) int {
-    s_bytes := []byte(s)
-    s_len := len(s_bytes)
-    t_bytes := []byte(t)
-    t_len := len(t_bytes)
-    if s_len != t_len {
-        log.Fatal("hamming_distance not implemented for strings of differing length")
-    }
-    var distance int
-    for i := 0; i < s_len; i++ {
-        distance += int(pc[s_bytes[i]^t_bytes[i]])
-    }
-    return distance
+func hamming_distance(s_bytes, t_bytes []byte) int {
+	s_len := len(s_bytes)
+	t_len := len(t_bytes)
+	if s_len != t_len {
+		log.Fatal("hamming_distance not implemented for slices of differing length")
+	}
+	var distance int
+	for i := 0; i < s_len; i++ {
+		distance += int(pc[s_bytes[i]^t_bytes[i]])
+	}
+	return distance
 }
+
+func read_base64_file(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	r := base64.NewDecoder(base64.StdEncoding, f)
+	var res []byte
+	buffer := make([]byte, 1024)
+	for {
+		n, err := r.Read(buffer)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		if n == 0 {
+			break
+		}
+		res = append(res, buffer...)
+	}
+	return res, nil
+}
+
+// func guess_keysize(cyphertext []byte, n_delta int) int {
+//     best_score := math.Inf(0)
+//     best_keysize := 0
+//     for keysize := 2; keysize < 41; keysize++ {
+//         var delta int
+//         for d := 0; d < n_delta; d++ {
+//             x := d*keysize
+//             y := (d+1)*keysize
+//             z := (d+2)*keysize
+//             b1 := cyphertext[x:y]
+//             b2 := cyphertext[y:z]
+//             delta += hamming_distance(b1, b2)
+//         }
+//         score := float64(delta)/float64(keysize * n_delta)
+//         if score < best_score {
+//             best_score = score
+//             best_keysize = keysize
+//         }
+//     }
+//     return best_keysize
+// }
+
+type Pair struct {
+	Key   int
+	Value float64
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+func rankByScore(scores map[int]float64) PairList {
+	result := make(PairList, len(scores))
+	i := 0
+	for k, v := range scores {
+		result[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(result)
+	return result
+}
+
+func score_key_sizes(min, max int, cyphertext []byte) PairList {
+	scores := make(map[int]float64)
+	for keysize := min; keysize < max+1; keysize++ {
+		score := 0
+		for n := 0; n < 30; n++ {
+			if (n+2)*keysize > len(cyphertext) {
+				break
+			}
+			b1 := cyphertext[n*keysize : (n+1)*keysize]
+			b2 := cyphertext[(n+1)*keysize : (n+2)*keysize]
+			score += hamming_distance(b1, b2)
+		}
+		scores[keysize] = float64(score) / float64(keysize)
+	}
+	return rankByScore(scores)
+}
+
+func guess_xor_key(cyphertext []byte, keysize int) []byte {
+	key := make([]byte, keysize)
+	for i := 0; i < keysize; i++ {
+		var block []byte
+		for j := i; j < len(cyphertext); j += keysize {
+			block = append(block, cyphertext[j])
+		}
+		_, k, _ := solve_single_byte_xor(block)
+		key[i] = k
+	}
+	return key
+}
+
+func decode_repeating_key_xor(cyphertext []byte) ([]byte, []byte) {
+	var best_plaintext, best_key []byte
+	best_score := 0.0
+	for keysize := 2; keysize < 40; keysize++ {
+		key := guess_xor_key(cyphertext, keysize)
+		plaintext := repeating_key_xor(cyphertext, key)
+		score := letter_freq_correlation(string(plaintext))
+		if score > best_score {
+			best_score = score
+			best_plaintext = plaintext
+			best_key = key
+		}
+	}
+	return best_plaintext, best_key
+}
+
+// func find_key(cyphertext []byte) []byte {
+//   scores := score_key_sizes(2, 40, cyphertext)
+//   keysize := scores[0].Key
+//   log.Printf("Trying key size %d\n", keysize)
+//   key := guess_xor_key(cyphertext, keysize)
+//   return key
+// }
--- set1_test.go
+++ set1_test.go
@@ -1,61 +1,124 @@
 package set1
 
 import (
-    "testing"
+	"encoding/hex"
+	"fmt"
+	"testing"
 )
 
 func TestChallenge1(t *testing.T) {
-    input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    observed := hex_to_base64(input)
-    if observed != expected {
-        t.Errorf("hex_to_base64(%s) != %s", input, expected)
-    }
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	expected := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	observed := hex_to_base64(input)
+	if observed != expected {
+		t.Errorf("hex_to_base64(%s) != %s", input, expected)
+	}
 }
 
 func TestChallenge2(t *testing.T) {
-    s := "1c0111001f010100061a024b53535009181c"
-    k := "686974207468652062756c6c277320657965"
-    expected := "746865206b696420646f6e277420706c6179"
-    observed := fixed_xor(s, k)
-    if observed != expected {
-        t.Errorf("fixed_xor(%s,%s) != %s", s, k, expected)
-    }
+	s := "1c0111001f010100061a024b53535009181c"
+	k := "686974207468652062756c6c277320657965"
+	expected := "746865206b696420646f6e277420706c6179"
+	observed := fixed_xor(s, k)
+	if observed != expected {
+		t.Errorf("fixed_xor(%s,%s) != %s", s, k, expected)
+	}
 }
 
 func TestChallenge3(t *testing.T) {
-    s := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    expected := "Cooking MC's like a pound of bacon"
-    observed, _ := solve_single_byte_xor(s)
-    if observed != expected {
-        t.Errorf("solve_single_byte_xor(%s) != %s", s, expected)
-    }
+	s := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	expected := "Cooking MC's like a pound of bacon"
+	cyphertext, _ := hex.DecodeString(s)
+	observed, _, _ := solve_single_byte_xor(cyphertext)
+	if string(observed) != expected {
+		t.Errorf("solve_single_byte_xor(%s) != %s", s, expected)
+	}
 }
 
 func TestChallenge4(t *testing.T) {
-    filename := "data/4.txt"
-    expected := "Now that the party is jumping\n"
-    observed := detect_single_character_xor(filename)
-    if observed != expected {
-        t.Errorf("detect_single_character_xor(%s) != %s", filename, expected)
-    }
+	filename := "data/4.txt"
+	expected := "Now that the party is jumping\n"
+	observed := detect_single_character_xor(filename)
+	if observed != expected {
+		t.Errorf("detect_single_character_xor(%s) != %s", filename, expected)
+	}
 }
 
 func TestChallenge5(t *testing.T) {
-    input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-    expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" +
-        "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
-    observed := repeating_key_xor(input, "ICE")
-    if observed != expected {
-        t.Errorf("repeating_key_xor() failed")
-    }
+	input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" +
+		"a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+	observed := hex.EncodeToString(repeating_key_xor([]byte(input), []byte("ICE")))
+	if observed != expected {
+		t.Errorf("repeating_key_xor() failed")
+	}
 }
 
-func TestHammingDistance(t *testing.T) {
-    s1 := "this is a test"
-    s2 := "wokka wokka!!!"
-    expected := 37
-    if hamming_distance(s1, s2) != expected {
-        t.Errorf("hamming_distance(%s, %s) != %d", s1, s2, expected)
-    }
+func Test_hamming_distance(t *testing.T) {
+	s1 := "this is a test"
+	s2 := "wokka wokka!!!"
+	expected := 37
+	if hamming_distance([]byte(s1), []byte(s2)) != expected {
+		t.Errorf("hamming_distance(%s, %s) != %d", s1, s2, expected)
+	}
+}
+
+func Test_read_base64_file(t *testing.T) {
+	xs, err := read_base64_file("data/6.txt")
+	if err != nil {
+		t.Errorf("read_base64_file(): %v", err)
+		return
+	}
+	if len(xs) != 4096 {
+		t.Errorf("read_base64_file() unexpected length %d", len(xs))
+	}
+}
+
+// func Test_guess_keysize(t *testing.T) {
+//     cyphertext, err := read_base64_file("data/6.txt")
+//     if err != nil {
+//         t.Errorf("read_base64_file(): %v", err)
+//         return
+//     }
+//     for i := 1; i < 50; i++ {
+//         keysize := guess_keysize(cyphertext, i)
+//         fmt.Printf("%d: %d\n", i, keysize)
+//     }
+// }
+
+// func Test_score_key_sizes(t *testing.T) {
+//   cyphertext, err := read_base64_file("data/6.txt")
+//   if err != nil {
+//     t.Errorf("read_base64_file(): %v", err)
+//     return
+//   }
+//   scores := score_key_sizes(2, 40, cyphertext)
+//   for _, x := range scores {
+//     fmt.Printf("%2d %6.4f\n", x.Key, x.Value)
+//   }
+// }
+
+func Test_decode_repeating_key_xor(t *testing.T) {
+	// cyphertext, err := read_base64_file("data/6.txt")
+	// if err != nil {
+	//   t.Errorf("read_base64_file(): %v", err)
+	//   return
+	// }
+	// p, k := decode_repeating_key_xor(cyphertext)
+	// fmt.Printf("Key length: %d\n", len(k))
+	// fmt.Println(string(p))
+
+	plaintext := "There once was a woman who lived in a shoe she had so many children she didn't know what to do"
+	key := "secret"
+	cyphertext := repeating_key_xor([]byte(plaintext), []byte(key))
+	scores := score_key_sizes(2, 20, cyphertext)
+	for _, x := range scores {
+		fmt.Printf("%2d %6.4f\n", x.Key, x.Value)
+	}
+	fmt.Println(string(repeating_key_xor(cyphertext, []byte("secRet"))))
+	fmt.Println(string(guess_xor_key(cyphertext, 6)))
+	p, k := decode_repeating_key_xor(cyphertext)
+	fmt.Println(string(p))
+	fmt.Println(string(k))
+
 }
